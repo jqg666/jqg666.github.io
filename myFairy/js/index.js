f72366b6086c0bda394cda592a77d1095c845c8b
jqg666 <344741883@qq.com>
2017-12-30 23:46:53 +0800

    index

--- myFairy/js/index.js
+++ myFairy/js/index.js
@@ -24,7 +24,7 @@
      $(document).one('WeixinJSBridgeReady', function () {
         var audio = document.getElementById("mp3Btn");
         audio.play(); //执行一次
-        audio.setAttribute('src', '../img/yekongxing.mp3'); // 替换src
+        audio.setAttribute('src', '../common/js/yekongxing.mp3'); // 替换src
         audio.load();
     })
 
